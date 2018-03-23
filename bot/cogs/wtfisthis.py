895fefa88b82ea03af784143800566854269ac6d
biskette <bandit5k@gmail.com>
2018-03-23 01:20:25 +0000

    We'll build them on common ground, and how!

--- bot/cogs/wtfisthis.py
+++ bot/cogs/wtfisthis.py
@@ -8,9 +8,15 @@ options.add_argument('window-size=1280x720')
 
 driver = webdriver.Chrome(chrome_options=options)
 
+# Wikipedia is nice because its elements are named cohesively
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
+
+# Find search element
 search = driver.find_element_by_id('searchInput')
-search.send_keys('snakes')
+
+# We obviously need to get a broad list of "snakes" from this search term
+# Yet obviously not something scientific, unless we do some wild card random to please the gods
+search.send_keys('list of snakes')
 driver.find_element_by_id('searchButton').click()
 
 sleep(5)
