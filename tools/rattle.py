99016fb76546f4b61130d247f7e93f18cd0334b5
silicWulf <me@siliconwolf.pw>
2018-03-23 17:00:33 -0400

    Silenced the screams of pain from flake8

--- tools/rattle.py
+++ tools/rattle.py
@@ -9,37 +9,35 @@ def check_word(word, wordset, threshold=0.8):
         return word
     '''
       glossary:
-      g - shortest string
-      c - counter used to get similarity
-      l - length used to supplement similarity
+      shortest - shortest string
+      count - counter used to get similarity
       length_diff_multiplier - multiplier for extra characters
       mp_count - multiplier for the multiplier, increases on every extra space
-      s - string used as length-matched word
-      o - string used as *not* `o` (is longer)
+      shorter - string used as length-matched word
+      longer - string used as *not* `o` (is longer)
     '''
     for w in wordset:
-        g = get_shortest(w, word)
-        c = 0
-        length_diff_multiplier = 1.08  # multiplies any characters past the shortest max index incrementally
+        shortest = get_shortest(w, word)
+        count = 0
+        length_diff_multiplier = 1.08
         mp_count = 1
-        l = len(g)
-        s = ''
-        if g == word:
-            s = match_length(word, w)
-            o = w
+        shorter = ''
+        if shortest == word:
+            shorter = match_length(word, w)
+            longer = w
         else:
-            s = match_length(w, word)
-            o = word
+            shorter = match_length(w, word)
+            longer = word
 
-        for i, char in enumerate(o):
-            if char != s[i]:
+        for i, char in enumerate(longer):
+            if char != shorter[i]:
                 if char == '~':
-                    c += mp_count * length_diff_multiplier
+                    count += mp_count * length_diff_multiplier
                     mp_count += 1
                 else:
-                    c += 1
+                    count += 1
 
-        avg = c / len(s)
+        avg = count / len(shorter)
         avg = 1.0  - avg
         if avg >= threshold:
             yield avg, w
