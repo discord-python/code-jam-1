8716582175b0d3c0b09d839d977148e744df53b5
biskette <bandit5k@gmail.com>
2018-03-23 01:41:18 +0000

    We all live in the space age, coming down with road rage...

--- bot/cogs/wtfisthis.py
+++ bot/cogs/wtfisthis.py
@@ -16,9 +16,18 @@ search = driver.find_element_by_id('searchInput')
 
 # We obviously need to get a broad list of "snakes" from this search term
 # Yet obviously not something scientific, unless we do some wild card random to please the gods
-search.send_keys('list of snakes')
+search.send_keys('list of snakes by common name')
 driver.find_element_by_id('searchButton').click()
 
-sleep(5)
+# Now we build some cool shit to scrape a bunch of snakes!
+# From a FUCKING MASSIVE list of shite
+result = driver.find_element_by_class_name('mw-parser-output')
+
+# So we're pulling the UL out of the above div
+newresults = result.find_elements_by_tag_name("ul")
+
+# Now we're just iterating over that and pulling the text element to see it works
+for each in newresults:
+    print(each.text)
 
 driver.close()
