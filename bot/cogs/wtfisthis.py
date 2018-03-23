f7da5b82a0218e95c6d6ad760c86561de5bfff22
biskette <bandit5k@gmail.com>
2018-03-23 21:32:14 +0000

    Trimmed to necessary for integration

--- bot/cogs/wtfisthis.py
+++ bot/cogs/wtfisthis.py
@@ -1,22 +1,4 @@
-#!/usr/bin/env python3.6
-
-import aiohttp
-import async_timeout
-import asyncio
-
-
-async def wikiquery(params):
-    params['format'] = 'json'
-
-    async with aiohttp.ClientSession(headers={'User-Agent': 'DickButt v.10rc2'}) as cs:
-        async with async_timeout.timeout(20):
-            async with cs.get("https://en.wikipedia.org/w/api.php", params=params) as r:
-                result = await r.json()
-
-    return result
-
-
-async def contquery(params):
+async def cont_query(params):
     last_continue = {}
 
     while True:
@@ -37,14 +19,15 @@ async def contquery(params):
         last_continue = request['continue']
 
 
-async def main():
+async def get_snake_list():
     ambiguous = ["(disambiguation)", "Wikipedia:", "Help:", "Category:"]
-    result = contquery({'action': 'query', 'titles': 'list_of_snakes_by_common_name', 'prop': 'links', 'format': 'json'})
+
+    snake_list = []
+    result = cont_query({'action': 'query', 'titles': 'list_of_snakes_by_common_name', 'prop': 'links', 'format': 'json'})
     async for dicks in result:
         listed = dicks
         for item in listed:
             if not any(s in item['title'] for s in ambiguous):
-                print(item['title'])
+                snake_list.append(item['title'])
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+    return snake_list
