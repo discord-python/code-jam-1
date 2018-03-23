54fbeb060b2b2c996871edc4f01300809e208b16
Işık Kaplan <36948560+isik-kaplan@users.noreply.github.com>
2018-03-23 05:52:53 +0300

    First draft

--- bot/cogs/snakes.py
+++ bot/cogs/snakes.py
@@ -4,6 +4,11 @@ from typing import Any, Dict
 
 from discord.ext.commands import AutoShardedBot, Context, command
 
+import aiohttp
+import ast
+import async_timeout
+import random
+
 log = logging.getLogger(__name__)
 
 
@@ -15,19 +20,34 @@ class Snakes:
     def __init__(self, bot: AutoShardedBot):
         self.bot = bot
 
+    @staticmethod
+    def snake_url(name) -> str:
+        BASEURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles={}"
+        with open('snakes.txt') as s:
+            if name + '\n' in s:
+                return BASEURL.format("%".join("{:02x}".format(ord(c)) for c in name))
+        did_you_mean = []
+        with open('snakes.txt') as f:
+            for line in f:
+                if len(set(name.replaceI(' ', '')) & set(line.replace(' ', '')[:-2])) > 5:
+                    did_you_mean.append(line[:-2])
+        return BASEURL.format("%".join("{:02x}".format(ord(c)) for c in random.choice(did_you_mean)))
+
+    async def fetch(self, session, url):
+        async with async_timeout.timeout(10):
+            async with session.get(url) as response:
+                return await response.text()
+
     async def get_snek(self, name: str = None) -> Dict[str, Any]:
-        """
-        Go online and fetch information about a snake
-
-        The information includes the name of the snake, a picture of the snake, and various other pieces of info.
-        What information you get for the snake is up to you. Be creative!
-
-        If "python" is given as the snake name, you should return information about the programming language, but with
-        all the information you'd provide for a real snake. Try to have some fun with this!
-
-        :param name: Optional, the name of the snake to get information for - omit for a random snake
-        :return: A dict containing information on a snake
-        """
+        if name is None:
+            name = random.choice([x[-2] for x in open('snakes.txt').readlines()])
+        async with aiohttp.ClientSession() as session:
+            html = await self.fetch(session, self.snake_url(name))
+            html_to_dic = ast.literal_eval(html)
+            return {
+                "snake name": html_to_dic["title"],
+                "snake info": html_to_dic["extract"]
+            }
 
     @command()
     async def get(self, ctx: Context, name: str = None):
