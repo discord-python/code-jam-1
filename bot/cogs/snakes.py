82cfd2f0e154ca6170496bfe4f195507700cf56c
Rohan <roalleti@gmail.com>
2018-03-23 19:51:13 +0530

    added more cmds

--- bot/cogs/snakes.py
+++ bot/cogs/snakes.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 import logging
 from typing import Any, Dict
-
+import discord
+import random
 from discord.ext.commands import AutoShardedBot, Context, command
 
 log = logging.getLogger(__name__)
@@ -28,19 +29,51 @@ class Snakes:
         :return: A dict containing information on a snake
         """
 
-    @command()
+    @command(name="get")
     async def get(self, ctx: Context, name: str = None):
-        """
-        Go online and fetch information about a snake
+        if name=="cobra":
+            msg=""" A Cobra is a species of venomous snake in the
+                    family Elapidae, endemic to forests from India
+                    through Southeast Asia. This serpent is the
+                    world's longest venomous snake."""
+        return await ctx.send(msg)
+    """
+            Go online and fetch information about a snake
 
-        This should make use of your `get_snek` method, using it to get information about a snake. This information
-        should be sent back to Discord in an embed.
+            This should make use of your `get_snek` method, using it to get information about a snake. This information
+            should be sent back to Discord in an embed.
 
-        :param ctx: Context object passed from discord.py
-        :param name: Optional, the name of the snake to get information for - omit for a random snake
-        """
+            :param ctx: Context object passed from discord.py
+            :param name: Optional, the name of the snake to get information for - omit for a random snake
+    """
 
     # Any additional commands can be placed here. Be creative, but keep it to a reasonable amount!
+
+
+    @command(name="snakerandom")
+    async def SnakeRandom(self, ctx: Context):
+        #snakes=['Cobra','Python','Anaconda','Black Mamba','Rattle Snake']
+        randsnake=random.choice(['cobra','python','black mamba'])
+        print(randsnake)
+        embed = discord.Embed(
+            title="print_stuff",
+            description="You get a "+randsnake,
+            color=0x00ff00,
+        )
+
+        if randsnake=="python":
+
+            return await ctx.send("Your a lucky dude !", embed=embed)
+        elif randsnake=="cobra":
+            return await ctx.send("Good old cobra !", embed=embed)
+        elif randsnake.startswith("blac"):
+            return await ctx.send("Shiny liitle fella !", embed=embed)
+
+
+
+
+
+
 
 
 def setup(bot):
