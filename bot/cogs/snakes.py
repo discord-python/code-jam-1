3a372522a23b4aac5330ccd9dd71917841140a90
Andrew Schwartz <andrew@aschwartz.me>
2018-03-23 20:56:18 -0600

    clean up code - type hinting, spelling, URL formatting, etc. (thanks @1mn)

--- bot/cogs/snakes.py
+++ bot/cogs/snakes.py
@@ -35,20 +35,22 @@ class Snakes:
         :return: A dict containing information on a snake
         """
 
-    async def get_snek_qwant_json(self, snake_name):
+    async def get_snek_qwant_json(self, snake_name: str) -> str:
         """
         Gets the json from Unsplash for a given snake query
         :param snake_name: name of the snake
         :return: the full JSON from the search API
         """
-        url = "https://api.unsplash.com/search/photos?client_id" \
-              "={0}&query={1}+snake".format(os.environ.get("UNSPLASH_CLIENT_ID"), snake_name)
+        client_id = os.environ.get("UNSPLASH_CLIENT_ID")
+        url = (
+            "https://api.unsplash.com/search/photos?client_id={client_id}&query={snake_name}+snake"
+        )
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 response = await response.read()
                 return json.loads(response.decode("utf-8"))
 
-    async def get_snek_image(self, name):
+    async def get_snek_image(self, name: str) -> str:
         """
         Gets the URL of a snake image
         :param name: name of snake
@@ -75,7 +77,7 @@ class Snakes:
             embed.add_field(
                 name="Python (programming language)",
                 value="*Guido van Rossum*\n\n"
-                      "This language is neither dangerous nor venomous and be found in software globally",
+                      "This language is neither dangerous nor venomous and can be found in software globally",
                 inline=False
             )
             embed.set_image(url=await self.get_snek_image("python programming language"))
@@ -111,7 +113,7 @@ class Snakes:
             embed=em
         )
 
-    def get_snek_fact(self):
+    def get_snek_fact(self) -> str:
         with open('bot/cogs/resources/facts.json', 'r', encoding="utf8") as f:
             data = json.load(f)
         choice = random.randint(0, len(data['facts'])-1)
