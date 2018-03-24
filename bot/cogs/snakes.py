058cef2e8ce76f83327515c69688dbc71e2015b5
Rohan <roalleti@gmail.com>
2018-03-24 21:12:37 +0530

    some editing

--- bot/cogs/snakes.py
+++ bot/cogs/snakes.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 import logging
 from typing import Any, Dict
-import discord
 import random
-import time
 import wikipedia
+import discord
 from discord.ext.commands import AutoShardedBot, Context, command
 
 log = logging.getLogger(__name__)
+
 
 class Snakes:
     """
@@ -19,47 +19,41 @@ class Snakes:
 
     async def get_snek(self, name: str = None) -> Dict[str, Any]:
 
-     """
-        Go online and fetch information about a snake
+        """
+           Go online and fetch information about a snake
 
-        The information includes the name of the snake, a picture of the snake, and various other pieces of info.
-        What information you get for the snake is up to you. Be creative!
+           The information includes the name of the snake, a picture of the snake, and various other pieces of info.
+           What information you get for the snake is up to you. Be creative!
 
-        If "python" is given as the snake name, you should return information about the programming language, but with
-        all the information you'd provide for a real snake. Try to have some fun with this!
+           If "python" is given as the snake name, you should return information about the programming language, but with
+           all the information you'd provide for a real snake. Try to have some fun with this!
 
-        :param name: Optional, the name of the snake to get information for - omit for a random snake
-        :return: A dict containing information on a snake
-     """
+           :param name: Optional, the name of the snake to get information for - omit for a random snake
+           :return: A dict containing information on a snake
+        """
 
     @command(name="get")
     async def get(self, ctx: Context, name: str = None):
-        if name.lower()=="python":
-            name_rechange="Python Programming Language"
+        if name.lower() == "python":
+            name_rechange = "Python Programming Language"
         else:
-            name_rechange=name
-        test=wikipedia.page(name_rechange)
+            name_rechange = name
+        test = wikipedia.page(name_rechange)
 
-        embed=discord.Embed(
+        embed = discord.Embed(
             title=test.title,
-            description= wikipedia.summary(name_rechange, sentences=1),
+            description=wikipedia.summary(name_rechange, sentences=1),
             color=0x00ff00,
         )
         embed.add_field(name="Image", value=test.images[0], inline=False)
         return await ctx.send(embed=embed)
 
-
-
-
-
-
     # Any additional commands can be placed here. Be creative, but keep it to a reasonable amount!
 
-
     @command(name="snakerandom")
-    async def SnakeRandom(self, ctx: Context , name: str= None):
-        #snakes=['Cobra','Python','Anaconda','Black Mamba','Rattle Snake']
-        randsnake=random.choice(['cobra','python','black mamba'])
+    async def SnakeRandom(self, ctx: Context, name: str = None):
+        # snakes=['Cobra','Python','Anaconda','Black Mamba','Rattle Snake']
+        randsnake = random.choice(['cobra', 'python', 'black mamba'])
         print(randsnake)
         embed = discord.Embed(
             title="Snake Random !",
@@ -67,38 +61,31 @@ class Snakes:
             color=0x00ff00,
         )
 
-        embed.add_field(name="Result", value="You got yourself a "+randsnake, inline=False)
+        embed.add_field(name="Result", value="You got yourself a " + randsnake, inline=False)
         embed.add_field(name="Expectation", value=f"@{ctx.author} expected {name}", inline=False)
 
-        if randsnake=="python":
+        if randsnake == "python":
 
-            return await ctx.send("Your a lucky dude !",embed=embed)
-        elif randsnake=="cobra":
-            return await ctx.send("Good old cobra !",embed=embed)
+            return await ctx.send("Your a lucky dude !", embed=embed)
+        elif randsnake == "cobra":
+            return await ctx.send("Good old cobra !", embed=embed)
         elif randsnake.startswith("blac"):
-            return await ctx.send("Shiny liitle fella !",embed=embed)
-
+            return await ctx.send("Shiny liitle fella !", embed=embed)
 
     @command(name="randname")
-    async def RandName(self,ctx:Context , name: str=None):
+    async def RandName(self, ctx: Context, name: str = None):
 
-        snk=random.choice(['Cobra','Python','Anaconda','Viper','Mamba'])
-        snLen=len(snk)
+        snk = random.choice(['Cobra', 'Python', 'Anaconda', 'Viper', 'Mamba'])
+        snLen = len(snk)
         p = len(name)
-        result=""
-        front_back=1
-        if front_back==1:#so the users name is substring from the front and snake random substring from back
-            ran = random.randint(1,p-2)
-            ranSnk=random.randint(1,snLen-1)
-            result=name[ :ran] + snk[ranSnk: ]
-
-
-
+        result = ""
+        front_back = 1
+        if front_back == 1:  # so the users name is substring from the front and snake random substring from back
+            ran = random.randint(1, p - 2)
+            ranSnk = random.randint(1, snLen - 1)
+            result = name[:ran] + snk[ranSnk:]
 
         return await ctx.send(result)
-
-
-
 
 
 def setup(bot):
