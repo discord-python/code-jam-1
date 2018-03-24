fcaecfb293772e160772c7f8ec50a5990d2908e3
Hasan <hasanahmadcodes@gmail.com>
2018-03-24 11:12:51 +0000

    Added python message

--- bot/cogs/snakes.py
+++ bot/cogs/snakes.py
@@ -13,10 +13,23 @@ class Snakes:
     """
     Snake-related commands
     """
+    embed = {}
+    python_info = '''
+                    Python (Programming Language)
+                    \n
+                    Python is powerful... and fast;\n
+                    plays well with others;\n
+                    runs everywhere;\n
+                    is friendly & easy to learn;\n
+                    is Open.
+                    -------------------------------
+                    Created by: Guido Van Rossum \n 
+                    Founded: 20th of February, 1991 \n
+                    Official website: https://python.org
+                '''
 
     def __init__(self, bot: AutoShardedBot):
         self.bot = bot
-        self.site = 'https://en.wikipedia.org/wiki/List_of_snakes_by_common_name'
 
     async def get_snek(self, name: str = None) -> Dict[str, Any]:
         """
@@ -31,12 +44,32 @@ class Snakes:
         :param name: Optional, the name of the snake to get information for - omit for a random snake
         :return: A dict containing information on a snake
         """
-
+        '''
+        site = 'https://en.wikipedia.org/wiki/List_of_snakes_by_common_name'
         async with aiohttp.ClientSession() as session:
-            async with session.get(self.site) as resp:
+            async with session.get(site) as resp:
                 text = await resp.read()
+                snake = name
+                soup = BeautifulSoup(text, 'lxml')
+                soup.find(snake)
+        '''
 
-
+        if str(name).lower() == 'python':
+                '''
+                Python (Programming Language)
+                \n
+                Python is powerful... and fast;\n
+                plays well with others;\n
+                runs everywhere;\n
+                is friendly & easy to learn;\n
+                is Open.
+                -------------------------------
+                Created by: Guido Van Rossum \n 
+                Founded: 20th of February, 1991 \n
+                Official website: https://python.org
+                '''
+                name = self.python_info
+        return name
 
     @command()
     async def get(self, ctx: Context, name: str = None):
@@ -50,8 +83,8 @@ class Snakes:
         :param name: Optional, the name of the snake to get information for - omit for a random snake
         """
         # await ctx.send(BeautifulSoup(text, 'lxml').find("title"))
-        await ctx.send(self.get_snek(name))
-        await ctx.send(random.choice(["snake1", "snake2", "snake3"]))
+        await ctx.send(await self.get_snek(name))
+
         # Any additional commands can be placed here. Be creative, but keep it to a reasonable amount!
 
 
