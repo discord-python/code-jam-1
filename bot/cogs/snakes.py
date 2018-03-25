2c6af66883b2c113cc2c07afcb7e74d9c5c4bd11
kingdom5500 <kingsleymcdonald@mail.com>
2018-03-25 13:44:41 +0100

    Added snake_card command (kinda)

--- bot/cogs/snakes.py
+++ bot/cogs/snakes.py
@@ -1,17 +1,22 @@
 # coding=utf-8
 
+# This shit imports more than the USA D:
 import async_timeout
 import random
 import difflib
 import logging
-import urllib.parse
 import ast
+import os
+import urllib.parse
+from io import BytesIO
+from functools import partial
 
 import aiohttp
 
-from typing import Dict, List
-from discord import Embed
+from typing import Dict
+from discord import Embed, File
 from discord.ext.commands import AutoShardedBot, Context, command
+from PIL import Image, ImageOps
 
 
 log = logging.getLogger(__name__)
@@ -34,6 +39,16 @@ absolutely pain-free software distribution... *sigh*""",
 with open("bot/snakes.txt") as f:
     SNAKES = [line.strip() for line in f.readlines()]
 
+# I don't think this is the best way of doing it
+CARD = {
+    "top": Image.open("bot/cards/card_top.png"),
+    "frame": Image.open("bot/cards/card_frame.png"),
+    "bottom": Image.open("bot/cards/card_bottom.png"),
+    "backs": [
+        Image.open(f"bot/cards/backs/{file}") for file in os.listdir("bot/cards/backs")
+    ]
+}
+
 
 class Snakes:
     """
@@ -42,10 +57,11 @@ class Snakes:
 
     def __init__(self, bot: AutoShardedBot):
         self.bot = bot
+        self.session = aiohttp.ClientSession()
 
     @staticmethod
-    def parse_kwarg(kwarg: str):
-        """Return the value of a kwarg (needs manual assignment)"""
+    def parse_arg(kwarg: str):
+        """Return the value of a arg (needs manual assignment)"""
         try:
             items = kwarg.split("=")
             value = items[1]
@@ -62,73 +78,79 @@ class Snakes:
     @staticmethod
     def snake_url(name: str):
         """Get the URL of a snake"""
+        name = name.lower()
 
         def format_url(text: str):
             """Get the full URL with that snake :D"""
             return BASEURL.format(urllib.parse.quote_plus(text))
 
         # Check if the snake name is valid
-        if name.upper() in [name.upper() for name in SNAKES]:
+        if name in SNAKES:
             return format_url(name)
 
         # Get the most similar name if a match wasn't found
         return difflib.get_close_matches(name, SNAKES, n=5, cutoff=0)
 
     @staticmethod
-    async def fetch(session, url: str):
-        """Fetch the contents of a URL as a json"""
+    async def fetch_image(self, url: str):
+        """Fetch an image, return a BytesIO object"""
+        stream = BytesIO()
         async with async_timeout.timeout(10):
-            async with session.get(url) as response:
-                return await response.json()
+            async with self.session.get(url) as response:
+                stream.write(await response.read())
 
-    async def get_snek(self, name: str = None, autocorrect: str = None, details: List[str] = None) -> Dict[str, str]:
+        stream.seek(0)
+        return stream
+
+    async def get_snek(self, name: str = None, autocorrect: str = None) -> Dict[str, str]:
         """If a name is provided, this gets a specific snake. Otherwise, it gets a random snake."""
 
-        autocorrect = self.parse_kwarg(autocorrect) or False
-        details = self.parse_kwarg(details) or []
+        autocorrect = self.parse_arg(autocorrect) or False
 
+        # Special cases
         if name is None:
             name = random.choice(SNAKES)
-
-        if name.upper() == "PYTHON":
+        elif name.lower() == "python":
             return PYTHON
 
-        # Get snake information
-        async with aiohttp.ClientSession() as session:
-            url = self.snake_url(name)
+        url = self.snake_url(name)
 
-            if isinstance(url, list):
-                if autocorrect:
-                    return await self.get_snek(url[0])
-
-                return {
-                    "name": "Oops!",
-                    "info": "We couldn't find that snake, but here are some with similar names:\n\n" + "\n".join(url),
-                }
-
-            # Get the content
-            response = await self.fetch(session, url)
-            page = response["query"]["pages"]
-            content = next(iter(page.values()))
-
-            # Parse the full-res image from the thumbnail
-            thumb = content.get("thumbnail", {}).get("source", FAILIMAGE)
-            image = "/".join(thumb.replace("thumb/", "").split("/")[:-1])
+        # Does some magic if the url is actually a list ^^
+        if isinstance(url, list):
+            if autocorrect:
+                return await self.get_snek(url[0])
 
             return {
-                "name": content["title"],
-                "info": content.get("extract", "I don't know about that snake!"),
-                "image": image
+                "name": "Oops!",
+                "info": "We can't find that snake, but here are some similar names:\n\n" + "\n".join(url).title()
             }
 
-    @command()
-    async def get(self, ctx: Context, name: str = None, autocorrect: str = None, details: str = None):
+        # Get the content
+        async with async_timeout.timeout(10):
+            async with self.session.get(url) as response:
+                response = await response.json()
 
-        content = await self.get_snek(name, autocorrect, details)
-        # Just a temporary thing to make sure it's working
+        page = response["query"]["pages"]
+        content = next(iter(page.values()))
+
+        # Parse the full-res image from the thumbnail
+        thumb = content.get("thumbnail", {}).get("source", FAILIMAGE)
+        image = "/".join(thumb.replace("thumb/", "").split("/")[:-1])
+
+        return {
+            "name": content["title"],
+            "info": content.get("extract", "I don't know about that snake!"),
+            "image": image
+        }
+
+    @command()
+    async def get(self, ctx: Context, name: str = None, autocorrect: str = None):
+
+        content = await self.get_snek(name, autocorrect)
+
         embed = Embed(
             title=content["name"],
-            description=content["info"][:1970],
+            description=content["info"][:2000],  # May consider cutting it shorter. 'Rattler' is a long one.
             color=0x7289da
         )
 
@@ -138,6 +160,75 @@ class Snakes:
         await ctx.send(embed=embed)
 
     # Any additional commands can be placed here. Be creative, but keep it to a reasonable amount!
+
+    # Cards are here :D
+
+    @staticmethod
+    def generate_card(buffer: BytesIO) -> BytesIO:
+        """Generate a card from snake information"""
+
+        snake = Image.open(buffer)
+
+        # Get the size of the snake icon, configure the height of the image box (yes, it changes)
+        icon_width = 347  # Hardcoded, not much i can do about that
+        icon_height = int((icon_width / snake.width) * snake.height)
+        frame_copies = icon_height // CARD['frame'].height + 1
+        snake.thumbnail((icon_width, icon_height))
+
+        # Get the dimensions of the final image
+        main_height = icon_height + CARD['top'].height + CARD['bottom'].height
+        main_width = CARD['frame'].width
+
+        # Start creating the final image
+        foreground = Image.new("RGBA", (main_width, main_height), (0, 0, 0, 0))
+        foreground.paste(CARD['top'], (0, 0))
+
+        # Generate the frame borders to the correct height
+        for offset in range(frame_copies):
+            position = (0, CARD['top'].height + offset * CARD['frame'].height)
+            foreground.paste(CARD['frame'], position)
+
+        # Add the image and bottom part of the image
+        foreground.paste(snake, (36, CARD['top'].height))  # Also hardcoded :(
+        foreground.paste(CARD['bottom'], (0, CARD['top'].height + icon_height))
+
+        # Setup the background
+        back = random.choice(CARD['backs'])
+        back_copies = main_height // back.height + 1
+        full_image = Image.new("RGBA", (main_width, main_height), (0, 0, 0, 0))
+
+        # Generate the tiled background
+        for offset in range(back_copies):
+            full_image.paste(back, (16, 16 + offset * back.height))
+
+        full_image.paste(foreground, (0, 0), foreground)
+
+        # Get the image contents as a BufferIO object
+        buffer = BytesIO()
+        full_image.save(buffer, 'PNG')
+        buffer.seek(0)
+
+        return buffer
+
+    @command()
+    async def snake_card(self, ctx: Context, name: str = None, autocorrect: str = 'True'):
+        content = await self.get_snek(name, autocorrect)
+
+        stream = BytesIO()
+        async with async_timeout.timeout(10):
+            async with self.session.get(content['image']) as response:
+                stream.write(await response.read())
+
+        stream.seek(0)
+
+        async with ctx.typing():
+            func = partial(self.generate_card, stream)
+            final_buffer = await self.bot.loop.run_in_executor(None, func)
+
+        await ctx.send(
+            f"A wild {content['name'].title()} appears!",
+            file=File(final_buffer, filename=content['name'].replace(" ", "") + ".png")
+        )
 
 
 def setup(bot):
