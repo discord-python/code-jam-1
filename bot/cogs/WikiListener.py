b5aa9aa099aa6b2c312ac63d1321414829909286
mushydev <mushfikur0@gmail.com>
2018-03-25 18:52:07 +0100

    Added some docstrings. Pep8 stuffs

--- bot/cogs/WikiListener.py
+++ bot/cogs/WikiListener.py
@@ -1,15 +1,6 @@
-import asyncio
-
-import aiohttp
-
 import json
 
-import ast
-
-import pprint
-
-import re
-
+import aiohttp
 
 """
 
@@ -19,12 +10,14 @@ Handle requests and grab and returns detail from wikipedia using MediaWiki.
 
 
 async def get_json(url):
+    """ Returns the text from request to url """
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             return await resp.text()
 
 
 def get_all_snek():
+    """" Gets all valid snake from a text file """
     file_save = open("bot/snakedata/common-snake-names.txt", "r")
     snake_text = file_save.read()
     snake_names = snake_text.split("\n")
@@ -33,20 +26,23 @@ def get_all_snek():
 
 
 async def get_raw_json(name):
-    raw_json = await get_json(f'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|pageimages&exintro=&explaintext=&titles={name}&redirects=1')
-    return raw_json
+    """ Returns a dictionary from the JSON grabbed from MediaWiki API. """
+    api_url = f'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|pageimages&exintro=&explaintext=&titles={name}&redirects=1'
+    raw_json = await get_json(api_url)
+    clean_json = json.loads(raw_json)
+    return clean_json
 
 
 async def get_snek_description(name: str = None):
-    json_file = await get_raw_json(name)
-    clean_json = json.loads(json_file)
-    return list(clean_json['query']['pages'].values())[0]['extract']
+    """ Returns 'extract' text from JSON, using a dictionary """
+    json_dict = await get_raw_json(name)
+    return list(json_dict['query']['pages'].values())[0]['extract']
 
 
 async def get_snek_scientific(name: str = None):
-    json_file = await get_raw_json(name)
-    clean_json = json.loads(json_file)
-    scientific = list(clean_json['query']['pages'].values())[0]['title']
+    """ Returns 'title' text from JSON, using a dictionary """
+    json_dict = await get_raw_json(name)
+    scientific = list(json_dict['query']['pages'].values())[0]['title']
     if scientific == name:
         return None
     else:
@@ -54,17 +50,14 @@ async def get_snek_scientific(name: str = None):
 
 
 async def get_snek_thumbnail(name: str = None):
-    json_file = await get_raw_json(name)
-    clean_json = json.loads(json_file)
-    return list(clean_json['query']['pages'].values())[0]['thumbnail']['source']
+    """ Returns 'thumbnail' source from JSON, using a dictionary """
+    json_dict = await get_raw_json(name)
+    return list(json_dict['query']['pages'].values())[0]['thumbnail']['source']
 
 
 async def get_snek_url(name: str = None):
-    json_file = await get_raw_json(name)
-    clean_json = json.loads(json_file)
-    title = list(clean_json['query']['pages'].values())[0]['title']
-    title.replace(" ", "%20")
-
+    """ Returns the url of the snake searched """
+    json_dict = await get_raw_json(name)
+    title = list(json_dict['query']['pages'].values())[0]['title']
     url = f'https://en.wikipedia.org/wiki/{title}'
     return url
-    
