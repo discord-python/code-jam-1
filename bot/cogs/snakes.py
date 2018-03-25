b97736cce2ad766ffc6dfd225fc45bcc9adff5fa
SamuelHallam <samuel.hallam@icloud.com>
2018-03-25 14:12:53 +0100

    Added back string docs, also fixed some errors if snakes were not found

--- bot/cogs/snakes.py
+++ bot/cogs/snakes.py
@@ -19,24 +19,31 @@ class Snakes:
         self.bot = bot
 
     async def get_snek(self, name: str = None) -> Dict[str, Any]:
+        """
+        Go online and fetch information about a snake
+        
+        The information includes the name of the snake, a picture of the snake, and various other pieces of info.
+        What information you get for the snake is up to you. Be creative!
+        
+        If "python" is given as the snake name, you should return information about the programming language, but with
+        all the information you'd provide for a real snake. Try to have some fun with this!
+        
+        :param name: Optional, the name of the snake to get information for - omit for a random snake
+        :return: A dict containing information on a snake
+        """
         with open('bot/db/snakes.json', 'r') as file:
                 snakes_dict = json.load(file)
 
-        if name is None or len(name) == 0:
-            _, snake_info = random.choice(list(snakes_dict.items()))
+        if not name:
+            _, snake = random.choice(list(snakes_dict.items()))
 
-        elif len(name) > 0:
+        elif name.lower() not in snakes_dict:
+            snake = "Not Found"
+
+        else:
             snake = snakes_dict[name.lower()]
-            if snake['name'] != "python":
-                snake_info = {
-                    'name': snake['name'],
-                    'description': snake['description'],
-                    'location': snake['location'],
-                    'venomous': snake['venomous'],
-                    'image': snake['image']
-                }
-            else:
-                snake_info = {
+            if snake['name'] == "python":
+                snake = {
                     'name': snake['name'],
                     'description': snake['description'],
                     'creator': snake['creator'],
@@ -44,29 +51,40 @@ class Snakes:
                     'image': snake['image']
                 }
 
-        return snake_info
+        return snake
 
     @command(name='get')
     async def get(self, ctx: Context, name: str = None):
         """
-        Shows information on different snakes.
+        Go online and fetch information about a snake
+        
+        This should make use of your `get_snek` method, using it to get information about a snake. This information
+        should be sent back to Discord in an embed.
+        
+        :param ctx: Context object passed from discord.py
+        :param name: Optional, the name of the snake to get information for - omit for a random snake
         """
+        snake = await self.get_snek(name)
 
-        snake_info = await self.get_snek(name)
+        if snake != "Not Found":
+            embed = Embed(
+                title=snake['name'].title(),
+                description=snake['description']
+            )
 
-        embed = Embed(
-            title=snake_info['name'].title(),
-            description=snake_info['description']
-        )
-
-        if snake_info['name'] != "python":
-            embed.add_field(name="Where can you find them?", value=snake_info['location'])
-            embed.add_field(name="Are they venomous?", value=snake_info['venomous'])
-            embed.set_image(url=snake_info['image'])
+            if snake['name'] != "python":
+                embed.add_field(name="Where can you find them?", value=snake['location'])
+                embed.add_field(name="Are they venomous?", value=snake['venomous'])
+                embed.set_image(url=snake['image'])
+            else:
+                embed.add_field(name="Who created it?", value=snake['creator'])
+                embed.add_field(name="When was it created?", value=snake['created'])
+                embed.set_thumbnail(url=snake['image'])
         else:
-            embed.add_field(name="Who created it?", value=snake_info['creator'])
-            embed.add_field(name="When was it created?", value=snake_info['created'])
-            embed.set_thumbnail(url=snake_info['image'])
+            embed = Embed(
+                title="Snake Not Found",
+                description="The snake you entered was not found."
+            )
 
         await ctx.send(embed=embed)
 
@@ -79,26 +97,33 @@ class Snakes:
         with open('bot/db/movies.json', 'r') as file:
             movies_dict = json.load(file)
 
-        if movie_name is None or len(movie_name) == 0:
+        if not movie_name:
             embed = Embed(
                 title="Snake Movies",
                 description="A list of snake movies.",
             )
 
             for movie in movies_dict.values():
-                embed.add_field(name=movie['title'], value=f"bot.movies('{movie['title'].lower()}')\n\n")
+                embed.add_field(name=movie['title'], value=f"bot.movies('{movie['title']}')\n\n")
 
             embed.set_thumbnail(url="https://i.imgur.com/dB38NwN.png")
 
-        elif len(movie_name) > 0:
-            embed = Embed(
-                title=movies_dict[movie_name]['title'],
-                description=movies_dict[movie_name]['description']
-            )
+        else:
+            movie_name = movie_name.lower()
+            if movie_name in movies_dict:
+                embed = Embed(
+                    title=movies_dict[movie_name]['title'],
+                    description=movies_dict[movie_name]['description']
+                )
 
-            embed.add_field(name="Director", value=movies_dict[movie_name]['director'])
-            embed.add_field(name="Release Date", value=movies_dict[movie_name]['released'])
-            embed.set_image(url=movies_dict[movie_name]['image'])
+                embed.add_field(name="Director", value=movies_dict[movie_name]['director'])
+                embed.add_field(name="Release Date", value=movies_dict[movie_name]['released'])
+                embed.set_image(url=movies_dict[movie_name]['image'])
+            else:
+                embed = Embed(
+                    title="Movie Not Found",
+                    description="The movie you entered was not found."
+                )
 
         await ctx.send(embed=embed)
 
