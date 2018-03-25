6b735ded005f2681912686f18826ca24fb2ddfe6
SamuelHallam <samuel.hallam@icloud.com>
2018-03-25 14:16:43 +0100

    Fixing flake8 issues

--- bot/cogs/snakes.py
+++ bot/cogs/snakes.py
@@ -21,13 +21,13 @@ class Snakes:
     async def get_snek(self, name: str = None) -> Dict[str, Any]:
         """
         Go online and fetch information about a snake
-        
+
         The information includes the name of the snake, a picture of the snake, and various other pieces of info.
         What information you get for the snake is up to you. Be creative!
-        
+
         If "python" is given as the snake name, you should return information about the programming language, but with
         all the information you'd provide for a real snake. Try to have some fun with this!
-        
+
         :param name: Optional, the name of the snake to get information for - omit for a random snake
         :return: A dict containing information on a snake
         """
@@ -57,7 +57,7 @@ class Snakes:
     async def get(self, ctx: Context, name: str = None):
         """
         Go online and fetch information about a snake
-        
+
         This should make use of your `get_snek` method, using it to get information about a snake. This information
         should be sent back to Discord in an embed.
         
